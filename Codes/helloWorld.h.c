e9a15316d37ae456b6236c705aaf2652346a8568
Ruan Lucena <ruanlucenaramos@gmail.com>
2022-07-24 10:27:39 -0300

    first test
    my first test in this repository using headers

---
+++ Codes/helloWorld.h.c
@@ -0,0 +1,6 @@
+
+// function test
+int hello()
+{
+	printf("Hello world! thats my first test in this repository whit C language\a");
+}
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 // you can put the header of the desired code here: #include <something.h>
+#include "C:\Users\ruan-\OneDrive\Documentos\GitHub\C_language\Codes\helloWorld.h.c"
 
 int main ()
 {
-
+    hello();
+    
     getchar();
     return 0;
 }
